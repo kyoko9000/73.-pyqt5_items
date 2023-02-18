188ba534d11695bfd038194999d89405943ea1fc
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-02-19 00:07:16 +0700

    update files

--- search_pyqt.py
+++ search_pyqt.py
@@ -1,24 +1,49 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout, QCompleter, QLineEdit, QApplication
+from PyQt5.QtWidgets import QWidget, QGridLayout, QCompleter, QLineEdit, QApplication, QPushButton
 import sys
 
 
 class Window(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        layout = QGridLayout()
-        self.setLayout(layout)
+        self.completer = None
+        self.layout = QGridLayout()
+        self.setLayout(self.layout)
 
-        # auto complete options
-        names = ["Apple", "Alps", "Berry", "Cherry"]
-        completer = QCompleter(names)
-
-        # create line edit and add auto complete
         self.lineedit = QLineEdit()
-        self.lineedit.setCompleter(completer)
-        layout.addWidget(self.lineedit, 0, 0)
+        self.layout.addWidget(self.lineedit, 0, 0)
+
+        self.names = ["Apple", "Alps", "Berry", "Cherry"]
+        self.completer = QCompleter(self.names)
+        model = self.completer.model()
+        model.setStringList(self.names)
+        self.lineedit.textChanged.connect(self.result)
+        self.completer.activated.connect(self.tip_balloon)
+
+    def result(self):
+        self.lineedit.setCompleter(self.completer)
+
+        text = self.lineedit.text()
+        names = ["Apple", "Alps", "Berry", "Cherry"]
+        lists = []
+        for i in names:
+            a = i.find(text)
+            if a >= 0:
+                lists.append(i)
+        print("result", lists)
+
+    def tip_balloon(self, text):
+        print("click", text)
 
 
 app = QApplication(sys.argv)
 screen = Window()
 screen.show()
 sys.exit(app.exec_())
+
+# names = ["Apple", "Alps", "Berry", "Cherry"]
+# lists = []
+# for i in names:
+#     a = i.find("er")
+#     if a >= 0:
+#         lists.append(i)
+# print(lists)
