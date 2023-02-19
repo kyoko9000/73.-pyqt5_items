21ed52b9a93d5a2143654e8a295f328da4711b4c
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-02-19 07:58:10 +0700

    update files

--- shadow_.py
+++ shadow_.py
@@ -1,8 +1,8 @@
 # importing libraries
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
 import sys
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont, QColor
+from PyQt5.QtWidgets import QMainWindow, QLabel, QGraphicsDropShadowEffect, QApplication
 
 
 class Window(QMainWindow):
@@ -11,50 +11,37 @@ class Window(QMainWindow):
         super().__init__()
         # setting title
         self.setWindowTitle("Python ")
-
         # setting geometry
         self.setGeometry(100, 100, 600, 400)
-
         # calling method
         self.UiComponents()
-
-        # showing all the widgets
-        self.show()
 
     # method for widgets
     def UiComponents(self):
         # creating label
         label = QLabel("Label", self)
-
         # setting font and size
         label.setFont(QFont('Arial', 100))
-
         # setting alignment
         label.setAlignment(Qt.AlignCenter)
-
         # setting geometry to the label
         label.setGeometry(50, 20, 400, 300)
-
         # setting border
         label.setStyleSheet("border : 10px solid black")
-
         # creating a QGraphicsDropShadowEffect object
         shadow = QGraphicsDropShadowEffect()
+        # set color for shadow
         shadow.setColor(QColor(63, 63, 63, 180))
+        # set offset for shadow
         shadow.setOffset(5, 5)
-
         # setting blur radius
         # shadow.setBlurRadius(15)
-
         # adding shadow to the label
         label.setGraphicsEffect(shadow)
 
 
 # create pyqt5 app
 App = QApplication(sys.argv)
-
-# create the instance of our Window
 window = Window()
-
-# start the app
+window.show()
 sys.exit(App.exec())
