f73d2bbab8e18700dcdbe43ab05a02fef427a5bf
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-02-21 20:50:20 +0700

    update files

--- pdf_.py
+++ pdf_.py
@@ -1,6 +1,8 @@
 import sys
+
+from PyQt5 import QtCore, QtPrintSupport, QtGui
 # pip install pyqt5
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QPushButton
 from gui import Ui_MainWindow
 
 
@@ -9,6 +11,59 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self)
+
+        self.uic.frame.setGeometry(QtCore.QRect(10, 10, 500, 300))
+        self.button = QPushButton("button")
+        self.button.clicked.connect(self.to_pdf)
+
+        self.w = QTableWidget(10, 10)
+        for i in range(10):
+            for j in range(10):
+                it = QTableWidgetItem("{}-{}".format(i, j))
+                self.w.setItem(i, j, it)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.w)
+        layout.addWidget(self.button)
+        self.uic.frame.setLayout(layout)
+
+    def to_pdf(self):
+        filename = "table.pdf"
+        model = self.w.model()
+
+        printer = QtPrintSupport.QPrinter(QtPrintSupport.QPrinter.PrinterResolution)
+        printer.setOutputFormat(QtPrintSupport.QPrinter.PdfFormat)
+        printer.setPaperSize(QtPrintSupport.QPrinter.A4)
+        printer.setOrientation(QtPrintSupport.QPrinter.Landscape)
+        printer.setOutputFileName(filename)
+
+        doc = QtGui.QTextDocument()
+
+        html = """<html>
+        <head>
+        <style>
+        table, th, td {
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        </style>
+        </head>"""
+        html += "<table><thead>"
+        html += "<tr>"
+        for c in range(model.columnCount()):
+            html += "<th>{}</th>".format(model.headerData(c, QtCore.Qt.Horizontal))
+
+        html += "</tr></thead>"
+        html += "<tbody>"
+        for r in range(model.rowCount()):
+            html += "<tr>"
+            for c in range(model.columnCount()):
+                html += "<td>{}</td>".format(model.index(r, c).data() or "")
+            html += "</tr>"
+        html += "</tbody></table>"
+        doc.setHtml(html)
+        doc.setPageSize(QtCore.QSizeF(printer.pageRect().size()))
+        doc.print_(printer)
 
 
 if __name__ == "__main__":
