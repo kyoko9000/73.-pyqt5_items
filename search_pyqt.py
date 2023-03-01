8a6cab4f03d1267cd0ec7e623dcdfe7c1aa73e4c
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2023-03-01 20:28:23 +0700

    update files

--- search_pyqt.py
+++ search_pyqt.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout, QCompleter, QLineEdit, QApplication, QPushButton
+from PyQt5.QtWidgets import QWidget, QGridLayout, QCompleter, QLineEdit, QApplication
 import sys
 
 
@@ -9,20 +9,27 @@ class Window(QWidget):
         self.layout = QGridLayout()
         self.setLayout(self.layout)
 
+        # create lineedit
         self.lineedit = QLineEdit()
         self.layout.addWidget(self.lineedit, 0, 0)
 
+        # make a list
         self.names = ["Apple", "Alps", "Berry", "Cherry"]
+        # new list with lowercase
         self.new_list = []
         for i in self.names:
             self.new_list.append(i.lower())
+        # create completer
         self.completer = QCompleter(self.new_list)
+        # action after put text
         self.lineedit.textChanged.connect(self.result)
+        # action after click text
         self.completer.activated.connect(self.tip_balloon)
 
     def result(self):
+        # set up completer
         self.lineedit.setCompleter(self.completer)
-
+        # search text by "find" function
         text = self.lineedit.text()
         lists = []
         for i in self.new_list:
@@ -43,7 +50,7 @@ sys.exit(app.exec_())
 # names = ["Apple", "Alps", "Berry", "Cherry"]
 # lists = []
 # for i in names:
-#     a = i.find("er")
+#     a = i.find("A")
 #     if a >= 0:
 #         lists.append(i)
 # print(lists)
